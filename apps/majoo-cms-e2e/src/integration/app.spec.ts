cc6e243518c1982b88270666eea84c40bd0f9dd5
Alfian Tryputranto <alfian.tryputranto@majoo.id>
2022-02-11 16:43:50 +0700

    setup nx workspace

---
+++ apps/majoo-cms-e2e/src/integration/app.spec.ts
@@ -0,0 +1,8 @@
+import { getGreeting } from '../support/app.po';
+
+describe('bookstore', () => {
+  beforeEach(() => cy.visit('/'));
+  it('should display welcome message', () => {
+    getGreeting().contains('Bookstore');
+  });
+});
